14171b82fec9015c0b6a3b73a4a60bc3df1d96fa
Nick Pisciotta <npisciotta@gmail.com>
2016-11-01 13:22:23 -0600

    Add POST and GET blob routes C and R portion of our CRUD app

--- expresswebapp/app.js
+++ expresswebapp/app.js
@@ -4,7 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var db = require("./model/db")
+var db = require("./model/db");
+var blob = require("./model/blobs");
 var index = require('./routes/index');
 var users = require('./routes/users');
 
---
+++ expresswebapp/routes/blobs.js
@@ -0,0 +1,68 @@
+var express = require("express"),
+    router  = express.Router(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
+router.use(bodyParser.urlencoded())
+router.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+   }
+}))
+
+router.route('/')
+  .get(function(req, res, next) {
+    mongoose.model('Blob').find({}, function(err, blobs) {
+      if (err) {
+        return console.error(err);
+      } else {
+        res.format({
+          html: function() {
+            res.render('blobs/index', {
+              title: "All my blobs",
+              "blobs": blobs
+            });
+          },
+          json: function() {
+            res.json(infophotos);
+          }
+        });
+      }
+    });
+  })
+  .post(function(req, res) {
+    var name = req.body.name;
+    var badge = req.body.badge;
+    var dob = req.body.dob;
+    var company = req.body.company;
+    var isloved = req.body.isloved;
+    mongoose.model("Blob").create({
+      name: name,
+      badge: badge,
+      dob: dob,
+      isloved: isloved
+    }, function (err, blob) {
+      if (err) {
+        res.send("There was a problem adding the information to the database")
+      } else {
+        console.log("Creating new blob:" + blob);
+        res.format({
+          html: function(){
+            res.location('blobs');
+            res.redirect("/blobs")
+          },
+          json: function() {
+            res.json(blob);
+          }
+        });
+      }
+    })
+  });
+
+  router.get("/new", function(req, res) {
+    res.render("blobs/new", {title: "Add New Blob"});
+  });
+  
